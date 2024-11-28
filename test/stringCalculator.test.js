d2694deabd057e916be380fcffb36b0f48c1c50e
Surya <35807369+suryashubham@users.noreply.github.com>
2024-11-28 12:19:57 +0530

    Update stringCalculator.test.js

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,2 +1,13 @@
 const { expect } = require('chai');
 const { add } = require('../src/stringCalculator');
+
+describe("String Calculators test suite",()=>{
+  it("Test handles empty string input",()=>{
+      let input = "";
+      let expectedValue = 0;
+      let actualValue = add(input);
+      expect(actualValue).to.equal(expectedValue);   
+  })
+  
+});
+
